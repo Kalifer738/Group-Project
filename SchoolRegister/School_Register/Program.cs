6ca754586935bd5a41c1022b0412d009225acccc
Kalifer738 <47210793+Kalifer738@users.noreply.github.com>
2020-02-29 20:24:12 +0200

    0.1 School Register
    A freshly new project with the 3 Nuget packages, 4 empty folders and a comment in Program.cs

---
+++ SchoolRegister/School_Register/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Register
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is a fresh new project containing the 3 Nuget packages, 4 empty folders and this comment in Program.cs Hai
+        }
+    }
+}
